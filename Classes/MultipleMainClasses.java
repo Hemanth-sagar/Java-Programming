9de4c92117f210903331a7eb1869781f07246954
Hemanth-sagar <hemanthsagar44@gmail.com>
2021-08-31 15:52:10 +0530

    Multiple main methods

---
+++ Classes/MultipleMainClasses.java
@@ -0,0 +1,24 @@
+class Clang
+{
+	public static void main(String args[])
+	{
+	 System.out.println("C is Pop");
+	 System.out.println("Middle level");
+	}
+}
+class Cpp
+{
+	public static void main(String args[])
+	{
+	 System.out.println("Cpp is OOP");
+	 System.out.println("Higher level");
+	}
+}
+class Java
+{
+	public static void main(String args[])
+	{
+	 System.out.println("Java is OOP");
+	 System.out.println("Higher level ");
+	}
+}
