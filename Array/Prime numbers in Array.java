e57c73d8d0bc2ed322be1a9f6a9ba93a0df29769
Hemanth-sagar <hemanthsagar44@gmail.com>
2021-09-12 15:06:00 +0530

    Array

---
+++ Array/Prime numbers in Array.java
@@ -0,0 +1,39 @@
+//no of prime numbers in it
+import java.util.*;
+class PrimeNums
+{  
+  public static void main(String ar[])
+  {
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the no of elements");
+	int n = sc.nextInt();
+	int a[]=new int[n];
+	System.out.println("enter the elements");
+	for(int i=0;i<n;i++)
+	{
+		System.out.println("enter the value");
+		a[i]=sc.nextInt();
+	}
+	int countprime=0;
+	for(int i=0;i<n;i++)
+	{
+	 int count=0;
+	 for(int j=2;j<a[i];j++)
+	 {
+	   if(a[i]%j==0)
+	   {
+	    count++;
+	    break;
+	   }
+	 }
+	 if(count==0)
+	 {
+	 countprime++;
+	 System.out.print("\t"+a[i]);
+	 }
+	}
+	System.out.println("\nTotal prime count is "+countprime);
+  }
+}
+	    
