db6758b48bd23a3c30e5efbdf463be67d9198121
Hemanth-sagar <hemanthsagar44@gmail.com>
2022-08-26 00:04:43 +0530

    lamda

---
+++ Java SE 8 Features/for each method.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OutClass {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(4,5,6,7);
+
+        //External for loops
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+
+        //enhanced for loop in previous versions
+        for(int i : list){
+            System.out.println(i);
+        }
+
+        //Java 1.8 for each method
+        list.forEach(i -> System.out.println(i));
+
+
+    }
+}
