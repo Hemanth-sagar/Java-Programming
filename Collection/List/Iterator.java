3bac6d0357c19973c891ac76aa4eefebca918e05
Hemanth-sagar <hemanthsagar44@gmail.com>
2023-04-05 13:34:37 +0530

    add, remove, set methods

--- Collection/List/Iterator.java
+++ Collection/List/Iterator.java
@@ -1,49 +1,61 @@
-        
+package pkg1;
 
-         /*
-            List Iterator:
-             next,hasNext,previous,hasPrevious
-             nextIndex,previousIndex,add,set,remove
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
-         */
+public class Class1 {
 
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+                        List Iterator:
+                        next,hasNext,previous,hasPrevious
+                        nextIndex,previousIndex,add,set,remove
+                 */
 
-        ListIterator it = list.listIterator();
+    List<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
 
-        while(it.hasNext()){
-            System.out.println(it.next());
+    ListIterator it = list.listIterator();
+
+    while(it.hasNext()){
+        System.out.println(it.next());
+    }
+
+    //to start with specific index
+    ListIterator it2 = list.listIterator(1);
+
+    //to start with end
+    ListIterator it3 = list.listIterator(list.size());
+
+    while(it3.hasPrevious()){
+        System.out.println(it3.previous());
+    }
+
+    //remove all null values
+    list.add(null);
+    list.add(5);
+    list.add(6);
+    list.add(null);
+    System.out.println(list);
+    
+    ListIterator it4 = list.listIterator();
+
+    while (it4.hasNext()){
+        if(it4.next()==null){
+            
+//            it4.set(0);
+//            it4.add(1);
+            it4.remove();
         }
-
-        //to start with specific index
-        ListIterator it2 = list.listIterator(1);
-
-        //to start with end
-        ListIterator it3 = list.listIterator(list.size());
-
-        while(it3.hasPrevious()){
-            System.out.println(it3.previous());
-        }
-
-        //remove all null values
-        list.add(null);
-        list.add(5);
-        list.add(6);
-        list.add(null);
-        System.out.println(list);
-
-        while (it.hasNext()){
-            if(it.next()==null){
-                it.remove();
-                it.set(0);
-                it.add(1);
-            }
-        }
-        System.out.println(list);
+    }
+    System.out.println(list);
 
 
+  }
 
+}
