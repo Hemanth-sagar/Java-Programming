e30feb2d156becc44f33b577c3b0addc77ea4a45
Hemanth-sagar <hemanthsagar44@gmail.com>
2023-04-06 14:33:09 +0530

    Create Student.java

---
+++ Collection/Sorting Collections/Comparator Interface/Student.java
@@ -0,0 +1,20 @@
+package pkg1;
+
+import java.util.Comparator;
+
+public class Student {
+	int id;
+	String name;
+	public Student() {
+		
+	}
+	public Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	 public String toString() {
+		 return id + " " + name;
+	 }
+	
+	
+}
